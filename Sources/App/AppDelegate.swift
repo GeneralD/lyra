bb4364c5958ae3fde1b30cc07f153c7dc2ce0d93
GeneralD <yumejustice@gmail.com>
2026-03-26 18:44:50 +0900

    refactor: simplify AppDelegate signal handling

--- Sources/App/AppDelegate.swift
+++ Sources/App/AppDelegate.swift
@@ -4,26 +4,18 @@ import AppKit
 public final class AppDelegate: NSObject, NSApplicationDelegate {
     private var router: AppRouter?
 
-    public override init() {
-        super.init()
-    }
-
     public func applicationDidFinishLaunching(_ notification: Notification) {
         let router = AppRouter()
         self.router = router
         Task {
             await router.start()
         }
-        setupSignalHandlers()
-    }
 
-    private func setupSignalHandlers() {
         for signalType in [SIGTERM, SIGINT] {
             signal(signalType, SIG_IGN)
             let source = DispatchSource.makeSignalSource(signal: signalType, queue: .main)
-            source.setEventHandler { [weak self] in
-                guard let self else { return }
-                router?.stop()
+            source.setEventHandler {
+                router.stop()
                 exit(0)
             }
             source.resume()
